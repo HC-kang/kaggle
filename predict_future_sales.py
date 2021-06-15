ec9bf8c8240966ef3afe9e539f7e6839229ed8ea
HC-kang <weston0713@gmail.com>
2021-06-16 07:52:25 +0900

    0615

--- predict_future_sales.py
+++ predict_future_sales.py
@@ -932,7 +932,55 @@ for i in models:
 
 
 
+##############
 
+
+import lightgbm as lgb
+feature_name = X_train.columns.tolist()
+
+params = {
+    'objective': 'mse',
+    'metric': 'rmse',
+    'num_leaves': 2 ** 8 -1,
+    'learning_rate': 0.005,
+    'feature_fraction': 0.8,
+    'bagging_fraction': 0.75,
+    'bagging_freq': 5,
+    'seed': 1,
+    'verbose': 1
+}
+feature_name_indexes = [ 
+                        'city', 
+                        'item_category_id',
+                        'cat_name', 
+                        'shop_id',
+]
+
+lgb_train = lgb.Dataset(X_train[feature_name], y_train)
+lgb_eval = lgb.Dataset(X_valid[feature_name], y_valid, reference=lgb_train)
+
+evals_result = {}
+gbm = lgb.train(
+        params, 
+        lgb_train,
+        num_boost_round=3000,
+        valid_sets=(lgb_train, lgb_eval), 
+        feature_name = feature_name,
+        categorical_feature = feature_name_indexes,
+        verbose_eval=50, 
+        evals_result = evals_result,
+        early_stopping_rounds = 10)
+
+
+
+
+Y_test = gbm.predict(X_test[feature_name]).clip(0, 20)
+
+submission = pd.DataFrame({
+    "ID": test.index, 
+    "item_cnt_month": Y_test
+})
+submission.to_csv('gbm_submission.csv', index=False)
 
 
 
